f0fe66c251bff508921e794522e284bb9fb8194f
ezralalonde <ezra@usefuliftrue.com>
2013-04-21 00:24:11 -0600

    Refactor if err != nil { panic(whatever) } into functions

--- main.go
+++ main.go
@@ -14,26 +14,26 @@ func main() {
 	for ii := 0; ii < sets; ii++ {
 		var col int
 		_, err := fmt.Scanf("%d", &col)
-		if err != nil {
-			panic("could not read number of columns")
-		}
+		check(err, "could not read number of columns")
 		var xx, yy []int
 		for jj := 0; jj < col; jj++ {
 			var val int
 			_, err := fmt.Scanf("%d", &val)
-			if err != nil {
-				panic("could not read row")
-			}
+			check(err, "could not read row")
 			xx = append(xx, val)
 		}
 		for jj := 0; jj < col; jj++ {
 			var val int
 			_, err := fmt.Scanf("%d", &val)
-			if err != nil {
-				panic("could not read row")
-			}
+			check(err, "could not read row")
 			yy = append(yy, val)
 		}
 		fmt.Printf("The maximum distance is %v\n\n", max.MaximumDistance(xx, yy))
+	}
+}
+
+func check(err error, str string) {
+	if err != nil {
+		panic(str)
 	}
 }
